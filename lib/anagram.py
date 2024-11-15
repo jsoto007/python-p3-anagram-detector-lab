bed794acbae4e71d1e6cdbb7fd8644b79196da87
Jose Soto <jsoto7087@gmail.com>
2024-11-15 14:10:37 -0500

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram: 
+  def  __init__(self, word, words=None):
+    self._word = word
+    self._words = words if words is not None else []
+
+  def get_word(self): 
+    return self._word
+  
+  def match(self, words):
+    sorted_word = sorted(self._word)
+    matches = [an for an in words if sorted(an) == sorted_word]
+    return matches
